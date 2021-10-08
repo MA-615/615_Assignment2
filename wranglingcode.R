6311770f6a90baba3ccfce3d2091a4828743c4e7
Lauren Temple <89480531+ltemps@users.noreply.github.com>
2021-10-07 22:04:45 -0400

    fixed reading data files

--- wranglingcode.R
+++ wranglingcode.R
@@ -1,31 +1,23 @@
 #Wrangling code
 
-pacman::p_load('dplyr', 'ggplot2', 'tidyr', 'knitr', 'stringr', 'tibble')
+pacman::p_load('dplyr', 'ggplot2', 'tidyr', 'knitr', 'stringr', 'tibble', 'readr')
 
-#assign data an easier name to call
-aid<- aid_given_2007_us
-#change column names of years to not include an X
-names(aid)<- str_replace(names(aid), "X", "")
-#change column name for country
-aidgiven<- rename(.data=aid, Country=ï..country)
+aid <- read_csv("aid_given_2007_us.csv")
+health <- read_csv("health_aid_given_percent_of_aid.csv")
+
 #tidy data
-aidgiven<- aidgiven %>% pivot_longer(!Country, names_to ="Year", values_to ="count")
-
-#repeat with health aid given data
-health<- health_aid_given_percent_of_aid
-names(health)<- str_replace(names(health), "X", "")
-healthaid<- rename(.data=health, Country=ï..country)
-healthaid<- healthaid %>% pivot_longer(!Country, names_to="Year", values_to="count")
+aidgiven<- aid %>% pivot_longer(!country, names_to ="Year", values_to ="count")
+healthaid<- health %>% pivot_longer(!country, names_to="Year", values_to="count")
 
 #left join the data and call it compareaid
 #used left join with aid on left because aid has more rows than healthaid
 #join by year and country because those variables are on both data sets
-compareaid<- left_join(aidgiven, healthaid, by=c("Year", "Country"))
+compareaid<- left_join(aidgiven, healthaid, by=c("Year", "country"))
 
 #rename the columns appropriately 
 compareaid<- rename(.data=compareaid, Aid=count.x, Health_Percent=count.y)
 #make compareaid into a kable
 compareaid%>%kable()
 #check that compareaid is tibble
-is_tibble(compareaid)
+#is_tibble(compareaid)
 
