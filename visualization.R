af011fe5f9d7767199d65e228f8153163b4098eb
Lauren Temple <89480531+ltemps@users.noreply.github.com>
2021-10-08 14:48:17 -0400

    with sourced files

--- visualization.R
+++ visualization.R
@@ -1,4 +1,4 @@
-Visualization
+#Visualization
 
 #create a function to filter the data set
 #want to be able to get a specific country and year span
@@ -6,23 +6,9 @@ Visualization
 #I chose to look at Canada from 1997 to 2006, the end of data collected
 plot<- function(a,b){
   {
-    Country<-a
-    data2<- filter(b, Country==a, Year>1996)
-    c<-ggplot(data=data2)+geom_col(aes(x=Year, y=Aid, fill=Health_Percent))+ggtitle(Country)
-    print(c)
-  }
-}
-
-plot("United States", compareaid)
-plot("Japan", compareaid)
-
-#same idea but with a bubble graph instead
-library(viridis)
-plot<- function(a,b){
-  {
-    Country<-a
-    data2<- filter(b, Country==a, Year>1996)
-    c<-ggplot(data=data2)+geom_point(aes(x=Year, y=Aid, size=Health_Percent))+scale_size(range = c(.5, 5), name="Health Aid Percentage")+ggtitle(Country)
+    country<-a
+    data2<- filter(b, country==a, Year>1996)
+    c<-ggplot(data=data2)+geom_col(aes(x=Year, y=Aid, fill=Health_Percent))+ggtitle(country)
     print(c)
   }
 }
@@ -31,17 +17,5 @@ plot("United States", compareaid)
 plot("Japan", compareaid)
 
 
-
-plot<- function(a,b){
-  {
-    Year<-a
-    data3<- filter(b, Year==a, Year>1996)
-    c<-ggplot(data=data2)+geom_point(aes(x=Year, y=Aid, size=Health_Percent))+scale_size(range = c(.5, 5), name="Health Aid Percentage")+ggtitle(Country)
-    print(c)
-  }
-}
-
-plot("United States", compareaid)
-plot("Japan", compareaid)
 
 
